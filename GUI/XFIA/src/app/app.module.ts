2ae6a77d1bc343d1397ee2869c2c0686719f6ebd
Luis Zuniga <lazh@estudiantec.cr>
2022-05-07 15:35:23 -0600

    Adding upper bar

--- GUI/XFIA/src/app/app.module.ts
+++ GUI/XFIA/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ GUI/XFIA/src/app/modules/tournaments/tournaments.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateTournamentComponent } from './create-tournament/create-tournament.component';
+
+
+
+@NgModule({
+  declarations: [
+    CreateTournamentComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TournamentsModule { }
