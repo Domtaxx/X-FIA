c31a8c4e041e49ff2e29ece7dcd296f18a548a83
Luis Zuniga <lazh@estudiantec.cr>
2022-05-08 20:46:37 -0600

    adding validations

--- GUI/XFIA/src/app/modules/tournaments/create/create.component.ts
+++ GUI/XFIA/src/app/modules/tournaments/create/create.component.ts
@@ -9,14 +9,24 @@ import { NetworkService } from 'src/app/services/network.service';
 })
 export class CreateComponent implements OnInit {
   tournamentForm=new FormGroup({
-    tournamentName:new FormControl('',Validators.required),
+    tournamentName:new FormControl('',[Validators.required,Validators.maxLength(30)]),
     initialDate:new FormControl('',Validators.required),
     finalDate: new FormControl('',Validators.required),
     initialTime: new FormControl('',Validators.required),
     finalTime:new FormControl('',Validators.required),
-    budget: new FormControl('',Validators.required),
+    budget: new FormControl('',[Validators.required,Validators.min(1)]),
     rules:new FormControl('',Validators.required)
   });
+  lab={
+    Name:'tournamentName',
+    dateI:'initialDate',
+    dateE:'finalDate',
+    timeS:'initilTime',
+    timeE:'finalTime',
+    budget:'budget',
+    rules:'rules'
+
+  }
   constructor(private backend:NetworkService) { }
 
   ngOnInit(): void {
@@ -25,12 +35,33 @@ export class CreateComponent implements OnInit {
     })
   }
   submit(){
-    var value=this.tournamentForm.get('tournamentName')?.value
+    //var valid=this.tournamentForm.get('tournamentName')?.valid
+    //this.tournamentForm.get('tournamentName')?.hasError('required')
+    //var value=this.tournamentForm.get('tournamentName')?.value
+    //Nombre Campeonato
+    var requiredNameValidator=this.tournamentForm.get('tournamentName')?.hasError('required');
+    var maxLenNameValidator=this.tournamentForm.get('tournamentName')?.hasError('maxLength')
+    if(requiredNameValidator){
+      alert("El nombre del campeonato no puede estar vacio")
+      return
+    }
+    if(maxLenNameValidator){
+      alert('El nombre del campeonato no puede ser mayor a 30 caracteres')
+      return
+    }
     console.log("imprimi")
-    console.log(value)
-    this.backend.get_request('',{}).subscribe((user)=>{
-      console.log(user)
-    })
+   // console.log(value)
+    
   }
+  /*
+  noValue(event:any){
+    var isEmpty=this.tournamentForm.get('tournamentName')?.hasError('required')
+    if(isEmpty){
+      console.log(event.target.getAttribute('name'))
+      //alert("El espacio no puede estar vacio")
+    }
+    
+  }
+  */
 
 }
