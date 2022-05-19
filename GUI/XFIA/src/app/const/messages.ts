10ffea63fa3d5121be8ee22416a9d563d97a925d
Luis Zuniga <lazh@estudiantec.cr>
2022-05-19 13:30:07 -0600

    Cleaning code

---
+++ GUI/XFIA/src/app/const/messages.ts
@@ -0,0 +1,27 @@
+export class alertMessages{
+    //http request status
+    public static successHeader='Exito';
+    public static rejected='No se ha podido procesar la solicitud';
+    //tournaments 
+    public static sucessTournament='Se ha agregado con éxito, la llave generada es ';
+    public static simultaneousTournaments='No se ha podido agregar el campeonato, recuerde que no es posible que existan dos campeonatos ocurriendo en fechas simultaneas'
+
+    //races 
+    public static raceSuccess='La carrera ha sido agregada al campeonato seleccionado' ;
+    public static duplicatedRaceName='el nombre indicado ya ha sido usado para otra carrera en el torneo seleccionado';
+    public static outsideBoundsRace='recuerde que las fechas de la carrera deben estar dentro de los limites del campeonato';
+    public static simultaneousRace='Ya existen carreras activas en el periodo indicado';
+
+    //Common Error
+    public static serverInterrupt='No se ha podido conectar con el servidor';
+
+    //Date/Time Fields Error
+    public static rejectedDateHeader="Fechas Invalidas"
+    public static invalidDateElapse="La fecha final debe ser mayor o igual a la fecha inicial";
+    public static invalidTimeElapsed="Si la competencia inicia y termina el mismo dia, la hora inicial debe ser menor a la fecha final"
+    public static inThePast="No es posible seleccionar fechas en el pasado"
+    public static invalidFieldsHeader="Errores en los campos";
+    public static invalidFieldsBody='Alguno de los campos indicados no cumple con las reglas, por favor revisar el texto bajo los cuadros'
+
+
+}
