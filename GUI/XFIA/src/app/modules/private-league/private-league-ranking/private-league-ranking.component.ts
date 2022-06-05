4e5adc09f916a53044eee1c127dba728cdaf4554
Luis Zuniga <lazh@estudiantec.cr>
2022-06-04 18:18:03 -0600

    Creating private league module

---
+++ GUI/XFIA/src/app/modules/private-league/private-league-ranking/private-league-ranking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-private-league-ranking',
+  templateUrl: './private-league-ranking.component.html',
+  styleUrls: ['./private-league-ranking.component.css']
+})
+export class PrivateLeagueRankingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ GUI/XFIA/src/app/modules/private-league/private-league-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PrivateLeagueRankingComponent } from './private-league-ranking/private-league-ranking.component';
+
+const routes: Routes = [
+{path:'ranking',component:PrivateLeagueRankingComponent},
+{path:'',redirectTo:'/ranking'}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PrivateLeagueRoutingModule { }
