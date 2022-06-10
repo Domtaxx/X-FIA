6eda792030fc6bc39c073ffc515f6214ee7dfa6a
Brian Wagemans <briwag88@estudiantec.cr>
2022-06-09 23:49:20 -0600

    publicLeague unit test

--- REST-API-XFIA.Tests/PublicLeagueVerificationTest.cs
+++ REST-API-XFIA.Tests/PublicLeagueVerificationTest.cs
@@ -20,6 +20,7 @@ namespace REST_API_XFIA.Tests
         
         private void init()
         {
+            users = new List<User>();
             u1 = new User();
             u1.Email = "Alvaov@estudiantec.cr";
             u2 = new User();
@@ -27,7 +28,9 @@ namespace REST_API_XFIA.Tests
             u3 = new User();
             u3.Email = "lazh@estudiantec.cr";
 
-
+            users.Add(u1);
+            users.Add(u2);
+            users.Add(u3);
 
             tour = new Tournament();
             tour.Budget = 45;
@@ -41,22 +44,23 @@ namespace REST_API_XFIA.Tests
         }
 
         [Fact]
-        public void VerifyIfSubTeamsNamesAreRepeatedTestValidate()
+        public void verifyUserPublicLeagueValidTest()
         {
-            User u1 = new User();
-
-            Tournament tour = new Tournament();
-            tour.Budget = 45;
-            tour.FinalDate = new DateTime(2001, 6, 6);
-            tour.InitialDate = new DateTime(2001, 5, 6);
-            tour.InitialHour = new TimeSpan(12, 0, 0);
-            tour.FinalHour = new TimeSpan(12, 0, 0);
-            tour.Key = "123Q56";
-            tour.Name = "TESTING 12345123";
-            tour.Rules = "algo de reglas";
-
-            Data_structures.AllUserInfo test= new();
-            
+            init();
+            Assert.Equal(0,PublicLeagueVerification.verifyUserPublicLeagueValid(tour, "briwag88@estudiantec.cr", users));
+            Assert.Equal(1, PublicLeagueVerification.verifyUserPublicLeagueValid(null, "briwag88@estudiantec.cr", users));
+            Assert.Equal(2, PublicLeagueVerification.verifyUserPublicLeagueValid(tour, "AREALA@estudiantec.cr", users));
         }
+        [Fact]
+        public void VerifyIfTournamentNotValidAndNumValidTest()
+        {
+            init();
+            Assert.False(PublicLeagueVerification.VerifyIfTournamentNotValidAndNumValid(tour, 1));
+            Assert.False(PublicLeagueVerification.VerifyIfTournamentNotValidAndNumValid(tour, 5));
+            Assert.True(PublicLeagueVerification.VerifyIfTournamentNotValidAndNumValid(tour, -1));
+            Assert.True(PublicLeagueVerification.VerifyIfTournamentNotValidAndNumValid(null, -1));
+            Assert.True(PublicLeagueVerification.VerifyIfTournamentNotValidAndNumValid(null, 61));
+        }
+
     }
 }
