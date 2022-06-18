027ddc8ad88ba6d8882e1b0d73300428931551cf
Brian Wagemans <briwag88@estudiantec.cr>
2022-06-17 22:17:09 -0600

    bugfixing

--- REST-API-XFIA/Data_structures/AllUserInfo.cs
+++ REST-API-XFIA/Data_structures/AllUserInfo.cs
@@ -11,17 +11,17 @@
         public string CountryName { get; set; } = null!;
         public string NameSubteam1 { get; set; } = null!;
         public string Car1 { get; set; } = null!;
-        public int pilot1Subteam1 { get; set; }
-        public int pilot2Subteam1 { get; set; }
-        public int pilot3Subteam1 { get; set; }
-        public int pilot4Subteam1 { get; set; }
-        public int pilot5Subteam1 { get; set; }
+        public string pilot1Subteam1 { get; set; }
+        public string pilot2Subteam1 { get; set; }
+        public string pilot3Subteam1 { get; set; }
+        public string pilot4Subteam1 { get; set; }
+        public string pilot5Subteam1 { get; set; }
         public string NameSubteam2 { get; set; } = null!;
         public string Car2 { get; set; } = null!;
-        public int pilot1Subteam2 { get; set; }
-        public int pilot2Subteam2 { get; set; }
-        public int pilot3Subteam2 { get; set; }
-        public int pilot4Subteam2 { get; set; }
-        public int pilot5Subteam2 { get; set; }
+        public string pilot1Subteam2 { get; set; }
+        public string pilot2Subteam2 { get; set; }
+        public string pilot3Subteam2 { get; set; }
+        public string pilot4Subteam2 { get; set; }
+        public string pilot5Subteam2 { get; set; }
     }
 }
